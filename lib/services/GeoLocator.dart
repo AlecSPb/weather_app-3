881a12efdbc606f1962a7c6d5d41637af3384692
Ushakov Andrey РИС-19-1б <65588137+leenzstra@users.noreply.github.com>
2020-07-13 14:55:38 +0500

    comments

--- lib/services/GeoLocator.dart
+++ lib/services/GeoLocator.dart
@@ -1,16 +1,25 @@
 import 'package:geolocator/geolocator.dart';
 
+//класс для доступа к геолокации
 class GeoLocator {
+  //позиция (класс содержит множество нужных полей)
   Position _position;
+  //ширина и долгота
   double _lat;
   double _lon;
-
+  
+  //геттеры
   double get lat => _lat;
   double get lon => _lon;
-
+  
+  //синхронная фунция, возвращаются позицию (либо текущую, если не заданы ширина и долгота, 
+  //либо по широте и долготе, либо, если не ВКЛ GPS, то по ум. ставится Москва)
+  
+  //Placemark - место (страна, город, деревня, улица и тп)
   Future<Placemark> getCurrentPosition({double lat, double lon}) async {
     Geolocator geolocator = Geolocator();
-
+    
+    //если заданы ширина и долгота
     if (lat != null && lon != null) {
       _lat = lat;
       _lon = lon;
@@ -20,7 +29,11 @@ class GeoLocator {
 
       return placemark[0];
     }
-
+    
+    //если не заданы, то пробуем получить текущие координаты через GPS
+    
+    //если разрешение не выдано (хотя бы ТОЛЬКО ВО ВРЕМЯ ИСПОЛЬЗОВАНИЯ ПРИЛОЖЕНИЯ, то каждый раз при запуске 
+    //будет требовать разрешение
     _position = await geolocator
         .getCurrentPosition(
             locationPermissionLevel: GeolocationPermission.locationWhenInUse,
@@ -30,14 +43,18 @@ class GeoLocator {
           locationPermissionLevel: GeolocationPermission.locationWhenInUse,
           desiredAccuracy: LocationAccuracy.medium);
     });
-
+    
+    //если GPS не сработал
+    //то ставим местопложение - Москва
     if (_position == null) {
-      //координаты москвы
+      //ширина и долгота москвы
       _lat = 55.7522;
       _lon = 37.6156;
       List<Placemark> placemark =
           await geolocator.placemarkFromCoordinates(_lat, _lon);
       return placemark[0];
+      //если GPS сработал
+      //то устанавливаем найденные ширину и долготу
     } else {
       _lat = _position.latitude;
       _lon = _position.longitude;
