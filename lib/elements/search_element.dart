0b536eea4919a3bda5c383141053b6ae9b310bd8
Ushakov Andrey РИС-19-1б <65588137+leenzstra@users.noreply.github.com>
2020-07-13 16:26:34 +0500

    comments

--- lib/elements/search_element.dart
+++ lib/elements/search_element.dart
@@ -2,20 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:weather_app/services/provider.dart';
 
+//Элемент поискового запроса (1 из результатов)
+
 class SearchElement extends StatelessWidget {
   SearchElement({Key key, this.index, this.description, this.lat, this.lon})
       : super(key: key);
 
+  //индекс в списке
   final int index;
+  //описание найденного местоположение
   final String description;
+  //ширина и долгота
   final double lat;
   final double lon;
 
   @override
   Widget build(BuildContext context) {
+    //цвет для текста
     Color color = Theme.of(context).textTheme.headline1.color;
+    //детектор жестов
     return GestureDetector(
       onTap: () {
+        //при нажатии делает запрос с текущими координатами и закрывает окно поиска
         Provider.of<WeatherProvider>(context, listen: false)
             .getWeatherCall(lat, lon);
         Navigator.pop(context);
