615019ec8f1a67d09262fa32a18c2da569daacd0
Ushakov Andrey РИС-19-1б <65588137+leenzstra@users.noreply.github.com>
2020-07-13 16:03:00 +0500

    comments

--- lib/services/themeProvider.dart
+++ lib/services/themeProvider.dart
@@ -2,19 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:weather_app/services/const.dart';
 
+//СМЕНА ТЕМЫ на светлую или темную
+
 class ThemeProvider extends ChangeNotifier {
+  //настройки темы
   ThemeData _themeData;
+  //коструктор
   ThemeProvider(this._themeData, this._isDark);
 
+  //булевая переменная, true - тем., false - светл.
   bool _isDark;
-
+  
+  //геттер темы
   ThemeData get themeData => _themeData;
 
+  //установка темы
   void _setThemeData(ThemeData themeData) {
     _themeData = themeData;
     notifyListeners();
   }
 
+  //смена темы
   changeTheme() async {
     if (!_isDark) {
       _setThemeData(darkTheme);
@@ -23,6 +31,7 @@ class ThemeProvider extends ChangeNotifier {
     }
 
     _isDark = !_isDark;
+    //запись настройки в память
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.setBool('theme', _isDark);
   }
