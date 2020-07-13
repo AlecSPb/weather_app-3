80219f9870b24888fab3c6a83957b4569e7873a7
Ushakov Andrey РИС-19-1б <65588137+leenzstra@users.noreply.github.com>
2020-07-13 14:56:47 +0500

    comments

--- lib/services/const.dart
+++ lib/services/const.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+// ФАЙЛ КОНСТАНТ
+
+//настройки темный темы
 final darkTheme = ThemeData(
     canvasColor: Colors.blueGrey[900],
     primaryColor: Colors.blueGrey[900],
@@ -8,6 +11,7 @@ final darkTheme = ThemeData(
     dividerColor: Colors.white30,
     textTheme: TextTheme(headline1: TextStyle(color: Colors.white)));
 
+//настройки светлой темы
 final lightTheme = ThemeData(
     canvasColor: Colors.white,
     primaryColor: Colors.white,
@@ -16,6 +20,7 @@ final lightTheme = ThemeData(
     dividerColor: Colors.black12,
     textTheme: TextTheme(headline1: TextStyle(color: Colors.black)));
 
+//тексты для info_page
 final String infoText1 =
     'Для исправной работы приложения требуется включить сервис GPS для получения геопозиции пользователя и доступ в Интернет. Задать свою геопозицию можно с помощью поиска. Для этого зайдите в меню и введите местоположение (требуется доступ в Интернет), выберите нужный пункт. Есть возмжность смены темы приложения.';
 final String infoText2 =
